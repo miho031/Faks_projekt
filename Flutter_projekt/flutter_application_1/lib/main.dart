62be6a1367dc0338253f8e0406730838d22e62d2
miho031 <kisicmiho@gmail.com>
2025-12-06 11:40:21 +0100

    Početak izrade aplikacije

---
+++ Flutter_projekt/flutter_application_1/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 87, 71, 3),
+        appBar: AppBar(
+          title: Text(
+            "FlexiSpace",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.yellow,
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+        ),
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+
+            padding: EdgeInsets.all(25),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 218, 199, 93),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Text(
+              "FlexiSpace",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
